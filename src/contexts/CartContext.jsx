d93ee758ba6a1bd631b09d69e30ad10f3d88f07d
Ethan Cornwill <ethancornwill@outlook.com>
2025-02-13 21:27:49 +1100

    Remove comments, to be added later

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -3,10 +3,8 @@ import React, { createContext, useState } from 'react';
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-	// State to store cart items
 	const [cartItems, setCartItems] = useState([]);
 
-	// Function to add an item to the cart with customization details
 	const addToCart = (item, customization) => {
 		setCartItems(prevItems => [...prevItems, { ...item, customization }]);
 	};
