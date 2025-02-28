f8aa86d454bb4ba73f176ccc06bce4d09d2e218c
finneh4249 <ethancornwill@outlook.com>
2025-02-28 17:47:50 +1100

    Add index file to export page components for easier imports

---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+
+import HomePage from './HomePage';
+import AboutPage from './AboutPage';
+import MenuPage from './MenuPage';
+import ContactPage from './ContactPage';
+import CartPage from './CartPage';
+import PageNotFound from './404Page';
+import ProfilePage from './ProfilePage';
+import AuthPage from './AuthPage';
+import AdminPage from './AdminPage';
+import StatusPage from './StatusPage';
+
+export {
+  HomePage,
+  AboutPage,
+  MenuPage,
+  ContactPage,
+  CartPage,
+  PageNotFound,
+  ProfilePage,
+  AuthPage,
+  AdminPage,
+  StatusPage
+};
