4ce5e4bf34e3d1951c58fb949a1070242acc1ba4
finneh4249 <ethancornwill@outlook.com>
2025-03-05 20:01:17 +1100

    Remove utils export from order history component

--- src/components/profile/order-history/index.js
+++ src/components/profile/order-history/index.js
@@ -4,4 +4,3 @@ export { default as OrderDetails } from './OrderDetails';
 export { default as EmptyState } from './EmptyState';
 export { default as LoadingState } from './LoadingState';
 export { default as ErrorState } from './ErrorState';
-export * from './utils';
