e11cd81ca7f5a5918d6513319bf6b1e019d45811
jevontrei <160815182+jevontrei@users.noreply.github.com>
2025-03-09 14:51:11 +1000

    unit test home page

--- vitest.config.js
+++ vitest.config.js
@@ -1,28 +1,30 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: "./src/test/setup.js",
-    coverage: {
-      provider: "istanbul",
-      reporter: ["text", "json", "html"],
-      reportsDirectory: "./coverage",
-      exclude: [
-        "node_modules/",
-        "src/setupTests.js",
-        "**/*.d.ts",
-        "**/*.test.{js,jsx,ts,tsx}",
-        "**/index.{js,jsx,ts,tsx}",
-      ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    testTimeout: 20000,
   },
-  define: {
-    "import.meta.env.VITE_API_URL": JSON.stringify("http://localhost:3000/api"),
-    "import.meta.env.VITE_NODE_ENV": JSON.stringify("test"),
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.js",
+    css: false,
+    include: ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    coverage: {
+      reporter: ["text", "json", "html"],
+      exclude: ["node_modules/"],
+    },
+    deps: {
+      optimizer: {
+        web: {
+          exclude: ["@mui/material"],
+        },
+      },
+    },
   },
 });
