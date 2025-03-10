336691958d98d580d99ad4b22b64657ca1b98f12
finneh4249 <ethancornwill@outlook.com>
2025-03-10 17:03:50 +1100

    Add BaseService class for streamlined API interactions and cache management

---
+++ src/api/services/BaseService.js
@@ -0,0 +1,46 @@
+import { apiHandler } from '../apiClient';
+import { invalidateCache } from '../../utils/cacheManager'; // Will need to adjust invalidateCache to accept a cache key
+
+class BaseService {
+  constructor(endpoint, cacheKey) {
+    this.endpoint = endpoint;
+    this.cacheKey = cacheKey;
+  }
+
+  async get(id = '') {
+    const requestEndpoint = id ? `${this.endpoint}/${id}` : this.endpoint;
+    return this.apiCall('get', requestEndpoint);
+  }
+
+  async create(data) {
+    return this.apiCall('post', `${this.endpoint}/new`, data, true);
+  }
+
+  async update(id, data) {
+    return this.apiCall('put', `${this.endpoint}/${id}`, data, true);
+  }
+
+  async delete(id) {
+    return this.apiCall('delete', `${this.endpoint}/${id}`);
+  }
+
+  async apiCall(method, endpoint, data = null, requiresAuth = false, authToken = null) {
+    try {
+      const result = await apiHandler({
+        method,
+        endpoint,
+        data,
+        requiresAuth,
+        authToken // Conditionally include authToken
+      });
+      if (this.cacheKey && (method === 'post' || method === 'put' || method === 'delete')) {
+        invalidateCache([this.cacheKey]);
+      }
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export default BaseService;
