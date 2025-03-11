4d2180a764e82bdaf02cf8a99f44b2d1525e6d77
finneh4249 <ethancornwill@outlook.com>
2025-03-11 22:02:24 +1100

    Remove Tailwind CSS plugin from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), sentryVitePlugin({
+  plugins: [react(), sentryVitePlugin({
     org: "ethan-cornwill",
     project: "javascript-react"
   })],
